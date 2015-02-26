801ba3a2a47a20d47c06330d6bbb21bf2498b4c0
Magnus Skjegstad <magnus@skjegstad.com>
2015-02-26 22:52:52 +0000

    Added DNS server, gitignore

--- gen.py
+++ gen.py
@@ -4,7 +4,6 @@ import os
 import hashlib
 import shutil
 
-print( "Indexing...")
 
 def make_url(path,filename):
     filename=filename.split("?")[0] # strip ?...
@@ -46,73 +45,96 @@ def stage_unikernel(meta,urlmap):
     shutil.copy("mirage/dispatch.ml", "%s/dispatch.ml" % path)
     replace_copy("mirage/config.ml", "%s/config.ml" % path, {'%IP%': meta['ip'], '%NETMASK%' : meta['netmask'], '%GATEWAY%' : meta['gw']})
 
-i = 10
-results = []
-urlmap = {}
-for f in os.walk('www.skjegstad.com'): 
-    path, dirs, files = f
-    if len(files) > 0:
-        for x in files:
-            result = process(path,x,"192.168.56.%d" % i, "255.255.255.0", "192.168.56.1")
+def process_static_webpage_path(rootdir,domain):
+    i = 10
+    results = []
+    urlmap = {}
+    for f in os.walk(rootdir): 
+        path, dirs, files = f
+        if len(files) > 0:
+            for x in files:
+                result = process(path,x,"192.168.56.%d" % i, "255.255.255.0", "192.168.56.1")
 
-            oldurl = '"http://' + result["oldurl"] + '"'
-            urlmap[oldurl] = '"http://' + result["ip"] + '/' + result["filename"] + '"'
-
-            oldurl = '"http://' + result["oldurl"] + '/"'
-            urlmap[oldurl] = '"http://' + result["ip"] + '/' + result["filename"] + '"'
-
-            oldurl = oldurl.replace("http://www.skjegstad.com", "")
-            if len(oldurl) > 1:
+                oldurl = '"http://' + result["oldurl"] + '"'
                 urlmap[oldurl] = '"http://' + result["ip"] + '/' + result["filename"] + '"'
 
-            oldurl = '"http://' + result["oldurl"] + '/' + result["filename"] + '"'
-            urlmap[oldurl] = '"http://' + result["ip"] + '/' + result["filename"] + '"'
-
-            oldurl = oldurl.replace("http://www.skjegstad.com", "")
-            if len(oldurl) > 1:
+                oldurl = '"http://' + result["oldurl"] + '/"'
                 urlmap[oldurl] = '"http://' + result["ip"] + '/' + result["filename"] + '"'
 
-            #urldir = '/' + '/'.join(path.split('/')[1:])
-            #if urldir != '/':
-                #urlmap[urldir] = result["ip"]
-            results.append(result)
-            print(result)
-            i = i + 1
-            
-print(sorted(urlmap.items(), lambda x,y: cmp(-len(x[0]), -len(y[0]))))
+                oldurl = oldurl.replace("http://%s" % domain, "")
+                if len(oldurl) > 1:
+                    urlmap[oldurl] = '"http://' + result["ip"] + '/' + result["filename"] + '"'
 
-for m in results:
-    stage_unikernel(m, urlmap)
+                oldurl = '"http://' + result["oldurl"] + '/' + result["filename"] + '"'
+                urlmap[oldurl] = '"http://' + result["ip"] + '/' + result["filename"] + '"'
 
-with open("Makefile", "w") as makefile:
-    # all
-    targets = []
-    for r in results:
-        targets.append("staging/%s/mir-www.xen" % (r['unikernel']))
-    makefile.write(".PHONY: all clean run destroy\n\n")
-    makefile.write("all: " + ' '.join(targets) + "\n\n")
+                oldurl = oldurl.replace("http://%s" % domain, "")
+                if len(oldurl) > 1:
+                    urlmap[oldurl] = '"http://' + result["ip"] + '/' + result["filename"] + '"'
 
-    # make targets
-    for r in results:
-        makefile.write("staging/%s/mir-www.xen: staging/%s/htdocs/%s\n\tcd staging/%s ; mirage configure --xen\n\tcd staging/%s ; make\n\n" % (r['unikernel'], r['unikernel'], r['filename'], r['unikernel'], r['unikernel'])) 
+                results.append(result)
+                #print(result)
+                i = i + 1
+                
+    #print(sorted(urlmap.items(), lambda x,y: cmp(-len(x[0]), -len(y[0]))))
 
-    # clean
-    makefile.write("clean:\n")
-    for r in results:
-        makefile.write("\tcd staging/%s ; make clean\n" % (r['unikernel'])) 
+    for m in results:
+        stage_unikernel(m, urlmap)
 
-    # run
-    makefile.write("run:\n")
-    for r in results:
-        makefile.write("\tcd staging/%s ; sudo xl create www.xl memory=32 \"vif=['bridge=br0']\" \"name='%s'\"\n" % (r['unikernel'], r['unikernel']))
-    makefile.write("\tsudo xl list\n")
+    return results
 
-    # destroy
-    makefile.write("destroy:\n")
-    for r in results:
-        makefile.write("\tsudo xl destroy %s || true\n" % r['unikernel'])
-    makefile.write("\tsudo xl list\n")
+def create_makefile(metadata):
+    with open("Makefile", "w") as makefile:
+        # all
+        targets = []
+        for r in metadata:
+            targets.append("staging/%s/mir-www.xen" % (r['unikernel']))
+        makefile.write(".PHONY: all clean run destroy dns\n\n")
+        makefile.write("all: serve_dns " + ' '.join(targets) + "\n\n")
 
-print("Makefile generated. Type 'make' to build, 'make run' to run and 'make destroy' to stop.")
+        # dns server
+        makefile.write("serve_dns: serve_dns.ml\n")
+        makefile.write("\tocamlfind ocamlopt serve_dns.ml -o serve_dns -package lwt.unix -package dns -package dns.lwt -linkpkg\n\n")
+
+        # make targets
+        for r in metadata:
+            makefile.write("staging/%s/mir-www.xen: staging/%s/htdocs/%s\n\tcd staging/%s ; mirage configure --xen\n\tcd staging/%s ; make\n\n" % (r['unikernel'], r['unikernel'], r['filename'], r['unikernel'], r['unikernel'])) 
+
+        # clean
+        makefile.write("clean:\n")
+        makefile.write("\trm -f serve_dns.cmi serve_dns.cmx serve_dns.o serve_dns\n")
+        for r in metadata:
+            makefile.write("\tcd staging/%s ; make clean\n" % (r['unikernel'])) 
+
+        # run
+        makefile.write("run:\n")
+        for r in metadata:
+            makefile.write("\tcd staging/%s ; sudo xl create www.xl memory=32 \"vif=['bridge=br0']\" \"name='%s'\"\n" % (r['unikernel'], r['unikernel']))
+        makefile.write("\tsudo xl list\n")
+
+        # destroy
+        makefile.write("destroy:\n")
+        for r in metadata:
+            makefile.write("\tsudo xl destroy %s || true\n" % r['unikernel'])
+        makefile.write("\tsudo xl list\n")
+
+        #dns
+        makefile.write("dns: serve_dns\n")
+        makefile.write("\tsudo ./serve_dns\n")
+
+def create_zone(zonefile, domain, meta):
+    with open(zonefile, "w") as z:
+        z.write ("@   IN  SOA     ns.%s.  postmaster.%s. (1 600 600 600 60)\n" % (domain, domain))
+        z.write ("%s.\tIN\tCNAME\tindex.html.%s.\n" % (domain, domain))
+        for r in meta:
+            z.write ('%s\tIN\tA\t%s\n' % (r['url'], r['ip']))
+
+domain = 'www.skjegstad.com'
+print( "Indexing %s..." % domain)
+meta = process_static_webpage_path(domain, domain)
+create_makefile(meta)
+print("\nMakefile generated. Type 'make' to build, 'make run' to run and 'make destroy' to stop. Start DNS server with 'make dns'")
+create_zone("%s.zone" % domain, domain, meta)
+print("\nZone file generated in %s.zone." % domain)
 
 
---
+++ serve_dns.ml
@@ -0,0 +1,39 @@
+open Dns
+open Lwt
+
+let forward_resolve resolver _class _type _name =
+    Printf.printf "Forwarding query for %s\n%!" (Name.domain_name_to_string _name);
+    Dns_resolver_unix.resolve resolver _class _type _name >>= fun result ->
+    return (Some (Dns.Query.answer_of_response result ))
+
+let match_or_forward resolver zone ~src ~dst packet =
+    let open Packet in
+    match packet.questions with
+    | [] -> Lwt.return_none (* no questions, return none *)
+    | [q] -> begin (* one question, try zone first *)
+                zone ~src ~dst packet >>= fun zone_answer ->
+                match zone_answer with
+                | None -> forward_resolve resolver q.q_class q.q_type q.q_name (* no result from zone, just forward *)
+                | Some answer -> begin (* got answer, check return code *)
+                    match answer.Query.rcode with
+                    | Packet.NoError -> Printf.printf "Local match for %s\n%!" (Name.domain_name_to_string q.q_name);
+                                        return (Some answer)
+                    | _ -> forward_resolve resolver q.q_class q.q_type q.q_name
+                end
+    end
+    | _::_::_ -> Lwt.return_none (* multiple questions? too much for us... *)
+
+let () =
+    Lwt_main.run (
+        (* server address and port *)
+        let address = "127.0.0.1" in
+        let port = 53 in
+        (* create forward resolver using /etc/resolv.conf *)
+        Dns_resolver_unix.create () >>= fun resolver ->
+        (* process zone file *)
+        Dns_server_unix.eventual_process_of_zonefiles ["www.skjegstad.com.zone"] >>= fun zone ->
+        (* check db first, then fall back to resolver on error *)
+        let processor = (Dns_server.processor_of_process (match_or_forward resolver zone) :> (module Dns_server.PROCESSOR)) in
+        Printf.printf "DNS server now running at %s port %d...\n%!" address port;
+        Dns_server_unix.serve_with_processor ~address ~port ~processor
+    )
