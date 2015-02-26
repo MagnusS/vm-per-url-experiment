6fbc184f5f181ad03c03fb687da278ad5c7bca4d
Magnus Skjegstad <magnus@skjegstad.com>
2015-02-26 23:15:01 +0000

    Added license

--- create.py
+++ create.py
@@ -1,9 +1,23 @@
 #!/usr/bin/env python
+'''
+ Copyright (c) 2015 Magnus Skjegstad <magnus@skjegstad.com>
+
+ Permission to use, copy, modify, and distribute this software for any
+ purpose with or without fee is hereby granted, provided that the above
+ copyright notice and this permission notice appear in all copies.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+'''
 
 import os
 import hashlib
 import shutil
-
 
 def make_url(path,filename):
     filename=filename.split("?")[0] # strip ?...
@@ -94,6 +108,7 @@ def create_makefile(metadata):
 
         # dns server
         makefile.write("serve_dns: serve_dns.ml\n")
+        makefile.write("\topam install lwt dns\n")
         makefile.write("\tocamlfind ocamlopt serve_dns.ml -o serve_dns -package lwt.unix -package dns -package dns.lwt -linkpkg\n\n")
 
         # make targets
